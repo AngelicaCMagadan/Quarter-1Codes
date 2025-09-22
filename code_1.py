6ac61dedd60b831e9b8325f079b357bc6d643dcb
AngelicaCMagadan <maangelica.magadan@smc.pshs.edu.ph>
2025-09-22 23:23:06 +0800

    Add files via upload
    This code will let the user input three numbers, and display the sum of those numbers.

---
+++ code_1.py
@@ -0,0 +1,16 @@
+num_1 = 0.0
+num_2 = 0.0
+num_3 = 0.0
+
+print("Enter first number: ")
+num_1 = float(input())
+
+print("Enter second number: ")
+num_2 = float(input())
+
+print("Enter third number: ")
+num_3 = float(input())
+
+sum = num_1 + num_2 + num_3
+
+print(f"The sum of the three numbers is: {sum:.2f}")
